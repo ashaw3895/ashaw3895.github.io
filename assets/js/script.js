ac8221fd7f98031ef62b3e77d44c44c5a0435092
ashaw3895 <ayshaw@berkeley.edu>
2016-11-26 01:27:10 -0800

    yas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,53 @@
 $(document).ready(function() {
-  
+
 
 //Implement the "slide to left" when the user clicks on #carousel-next here
-  $('#carousel-next').click(function(){
-    var m=parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (m == -3840) {
-      return false;
-    }
-    else {
-      $('#carousel').css('margin-left',(m-960)+'px');
-    };
-  });
-
-  //Implement the "slide to right" when the user clicks on #carousel-prev here
-  $('#carousel-prev').click(function (){
-    var m=parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (m == 0) {
-    return false;
-    }
-    else {
-    $('#carousel').css('margin-left',(m+960)+'px')
-    };
-});
+//   $('#carousel-next').click(function(){
+//     var m=parseInt($('#carousel').css('margin-left').replace("px", ""));
+//     if (m == -3840) {
+//       return false;
+//     }
+//     else {
+//       $('#carousel').css('margin-left',(m-960)+'px');
+//     };
+//   });
+//
+//   //Implement the "slide to right" when the user clicks on #carousel-prev here
+//   $('#carousel-prev').click(function (){
+//     var m=parseInt($('#carousel').css('margin-left').replace("px", ""));
+//     if (m == 0) {
+//     return false;
+//     }
+//     else {
+//     $('#carousel').css('margin-left',(m+960)+'px')
+//     };
+// });
 // click on -> button:
 //     if at the end of carousel:
 //         set margin-left = 0
 //     else:
 //         set margin-left = current margin-left - 960
-$('carousel-next').click(function(){
-  var m=parseInt($('#carousel').css('margin-left').replace("px", ""));
-  if (m == 0) {
-  return false;
-  }
-  else {
-  $('#carousel').css('margin-left',(m-960)+'px')
-  };
-})
+// $('carousel-next').click(function(){
+//   var m=parseInt($('#carousel').css('margin-left').replace("px", ""));
+//   if (m == 0) {
+//   return false;
+//   }
+//   else {
+//   $('#carousel').css('margin-left',(m-960)+'px')
+//   };
+// })
 //sticky nav
+
+function parallax() {
+    /* YOUR CODE HERE */
+    var scrolled = $(window).scrollTop();
+    $('.parallax').css('top', -(scrolled * 0.6) + 'px');
+  }
 // code here
 var searchBarPosition = $(".nav").offset().top;
 
 $(window).scroll(function() {
+  parallax();
   var scrollHeight = $(window).scrollTop();
 
   if (scrollHeight > searchBarPosition) {
@@ -48,7 +55,8 @@ $(window).scroll(function() {
   } else {
     $(".nav").removeClass("search-bar-fixed");
   }
-});
+
+  });
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -57,7 +65,10 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
-}
+};
+
+
+
 
 // var searchBarPosition = $(".search-container").offset().top;
 
